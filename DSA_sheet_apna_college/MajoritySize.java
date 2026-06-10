7be2b77ffd3257b921f8dbc56735dc4a3015302a
akankshatripathi <akankshatripathi2602@gmail.com>
2026-06-10 15:36:56 +0530

    two sum sorted leetcode 167'

--- DSA_sheet_apna_college/MajoritySize.java
+++ DSA_sheet_apna_college/MajoritySize.java
@@ -5,7 +5,16 @@ public class MajoritySize {
         int count = 0;
         int candidate = nums[0];
         for (int i = 0; i < nums.length; i++) {
-            
+            if(count == 0){
+                candidate = nums[i];
+                count++;
+            }else if(nums[i] == candidate){
+                    count++;
+            }else{
+                count--;
+            }
+
         }
+        return candidate;
     }
 }
